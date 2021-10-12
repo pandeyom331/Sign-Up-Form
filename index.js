c79f3e9ae6da14d43308f5a5555d65b53a9e1a19
Deep9110 <99deepsundar@gmail.com>
2021-10-12 18:33:09 +0530

    Added button animation and Javascript effect to the webpage.

---
+++ index.js
@@ -0,0 +1,20 @@
+const button   = document.querySelector('.submit-button'),
+      stateMsg = document.querySelector('.pre-state-msg');
+
+const updateButtonMsg = function() {
+  button.classList.add('state-1', 'animated');
+  
+  setTimeout(finalButtonMsg, 2000);
+};
+
+const finalButtonMsg = function() {
+  button.classList.add('state-2');
+  
+  setTimeout(setInitialButtonState, 2000);
+};
+
+const setInitialButtonState = function() {
+  button.classList.remove('state-1', 'state-2', 'animated');
+};
+
+button.addEventListener('click', updateButtonMsg);
